76cdc7f7187a886768a8cd32a4b45b4caf60b321
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-11-07 01:46:33 +0100

    just commit - geomart for users [frontend with react]

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,9 +4,9 @@ const Loading = () => {
   return (
     <>
       <Navbar />
-      <div className="flex justify-center items-center h-screen">
+      {/* <div className="flex justify-center items-center h-screen">
         <div className="text-xl font-bold text-gray-900">Loading...</div>
-      </div>
+      </div> */}
     </>
   );
 };
